a4ff050bc99fcaa82ab604a3df02b36c18b8b86f
Nil <Nil@Lightning.fios-router.home>
2019-03-08 01:35:23 -0500

    comments added, redundant code removed

--- ParkingReservationSystem/src/date.java
+++ ParkingReservationSystem/src/date.java
@@ -2,32 +2,18 @@
  *
  * @author Nil
  */
-    import java.util.*;
-    import java.text.*;
      
     public class date {
-    private parkingSpot[][] parking = new parkingSpot[24][50];
+    private final parkingSpot[][] parking = new parkingSpot[24][50]; //creates a two dimensional array [hours][spot] for the instance of the date
     private int spot;
     private int time;
     private String name;
     private String date;
-    private String message = "Kappa";
+    private String message = "Kappa";//this is just a placeholder message if for some reason this returns to the user it basically means there is an error in the code
     private String message2 = "";
-     
-    DateFormat dateHour = new SimpleDateFormat("HH");
-    Date now = new Date();
-    String hour = dateHour.format(now);
-    int hr = Integer.parseInt(hour);
-     
-    int i2 = 0;
-     
-    DateFormat dateYear = new SimpleDateFormat("yyyy");
-    DateFormat dateMonth = new SimpleDateFormat("MM");
-    DateFormat dateDay = new SimpleDateFormat("dd");
-    Date now2 = new Date();
 
  
-public date ()
+public date ()// constructor for instance initializes two dimensional array
 {
     for(int i = 0; i<24;i++)
     {
@@ -40,56 +26,8 @@ public date ()
 }
      
      
-public String reserve(String name1, int time1, int spot1, String date1)
-{
-            Date now2 = new Date();
-	    String year = dateYear.format(now2);
-	    String month = dateMonth.format(now2);
-	    String day = dateDay.format(now2);
-	    int yr = Integer.parseInt(year);
-	    int mth = Integer.parseInt(month);
-	    int dy = Integer.parseInt(day);
-	    String month2 = date1.substring(0,2);
-	    String day2 = date1.substring(3,5);
-	    String year2 = date1.substring(6,10);
-	    int yr2 = Integer.parseInt(year2);
-	    int mth2 = Integer.parseInt(month2);
-	    int dy2 = Integer.parseInt(day2);
-	     
-	    {
-	    if(yr2 > yr)
-	    {
-	        i2 = 1;
-	    }
-	     
-	    else if(yr2 == yr)
-	    {
-	        if(mth2 > mth)
-	        {
-	            i2 = 1;
-	        }
-	         
-	        else if(mth2 == mth)
-	        {
-	            if(dy2 >= dy)
-	            {
-	                i2 = 1;
-	            }
-	             
-	        }
-	    }
-	    }
-    if(i2 == 0)
-    {
-        if(time1 < hr)
-        {
-            i2 = 1;
-        }
-    }
-     
-    if(i2 == 1)
-    {
-   
+public String reserve(String name1, int time1, int spot1, String date1) //method will reserve the spot at certain time on a given date, and sets the date for the instance.
+{  
     spot1--;
     if (parking[time1][spot1].getState().equalsIgnoreCase("Available"))
         {
@@ -101,69 +39,13 @@ public String reserve(String name1, int time1, int spot1, String date1)
         }
     else
         message = "Spot unavailable";
-    return message;
-    }
-     
-    else
-    {
-        message = "You cannot make a reservation on a date that has already passed.";
-        return message;
-     
-    }
-     
-     
+    return message;  
 }
  
  
-public String cancel(String name1, int time1, int spot1, String Date1)
+public String cancel(String name1, int time1, int spot1, String Date1)//will cancel reservation if it exists at the time and spot at given date
 {
-            Date now2 = new Date();
-	    String year = dateYear.format(now2);
-	    String month = dateMonth.format(now2);
-	    String day = dateDay.format(now2);
-	    int yr = Integer.parseInt(year);
-	    int mth = Integer.parseInt(month);
-	    int dy = Integer.parseInt(day);
-	    String month2 = Date1.substring(0,2);
-	    String day2 = Date1.substring(3,5);
-	    String year2 = Date1.substring(6,10);
-	    int yr2 = Integer.parseInt(year2);
-	    int mth2 = Integer.parseInt(month2);
-	    int dy2 = Integer.parseInt(day2);
-	     
-	    {
-	    if(yr2 > yr)
-	    {
-	        i2 = 1;
-	    }
-	     
-	    else if(yr2 == yr)
-	    {
-	        if(mth2 > mth)
-	        {
-	            i2 = 1;
-	        }
-	         
-	        else if(mth2 == mth)
-	        {
-	            if(dy2 >= dy)
-	            {
-	                i2 = 1;
-	            }
-	             
-	        }
-	    }
-	    }
-if(i2 == 0)
-{
-    if(time1 < hr)
-    {
-        i2 = 1;
-    }
-}
- 
-if(i2 == 1)
-{
+            
     spot1--;
     System.out.println(parking[time1][spot1].getState().equalsIgnoreCase("Reserved"));
         if(parking[time1][spot1].getState().equalsIgnoreCase("Reserved"))
@@ -185,65 +67,12 @@ if(i2 == 1)
              
          
         }
-    }
-         
-        else
-        {
-            message = "You cannot make a reservation on a date that has already passed.";
-            return message;
-         
-        }
+  
     }
  
  
-public String extend(String name1,int time1, int spot1, String Date1){    
-Date now2 = new Date();
-String year = dateYear.format(now2);
-String month = dateMonth.format(now2);
-String day = dateDay.format(now2);
-int yr = Integer.parseInt(year);
-int mth = Integer.parseInt(month);
-int dy = Integer.parseInt(day);
-String month2 = Date1.substring(0,2);
-String day2 = Date1.substring(3,5);
-String year2 = Date1.substring(6,10);
-int yr2 = Integer.parseInt(year2);
-int mth2 = Integer.parseInt(month2);
-int dy2 = Integer.parseInt(day2);
- 
+public String extend(String name1,int time1, int spot1, String Date1){   //will extend previous reservation if possible by one hour
 
-if(yr2 > yr)
-{
-    i2 = 1;
-}
- 
-else if(yr2 == yr)
-{
-    if(mth2 > mth)
-    {
-        i2 = 1;
-    }
-     
-    else if(mth2 == mth)
-    {
-        if(dy2 >= dy)
-        {
-            i2 = 1;
-        }
-         
-    }
-}
-
-    if(i2 == 0)
-    {
-        if(time1 < hr)
-        {
-            i2 = 1;
-        }
-    }
-     
-    if(i2 == 1)
-    {
         int timeDes = time1;
         spot = spot1-1;
          
@@ -276,32 +105,27 @@ else if(yr2 == yr)
         {
             message = "Unable to extend a non-existent reservation.";
         }
-     
-    }
-     
-    else
-    {
-        message = "You cannot make a reservation on a date that has already passed.";
-    }
     return message;
      
 }
  
-public String getDate()
+public String getDate()//returns date important to make sure more then one date (class) doesn't exist when looping through the arraylist in the server
 {
     return date;
 }
  
  
-public String view(String date1){
+public String view(String date1){ //will return all reservations on given date
 // Requires If-Else Statement to Return Occupied Based on Time
     for(int i = 0; i<24;i++)
     {
         for (int j=0; j<50;j++)
         {
-            int k = j + 1;
+            int k = j + 1; // spots are 1-50 but are stored from 0-49
             if(parking[i][j].getState().equalsIgnoreCase("Reserved"))
-            { message2 = "";
+            { 
+            if(!message2.equalsIgnoreCase(""))
+            {message2 = message2 + ", ";}       
             message2 = message2 + " Spot " + k + " is reserved at " + i + " " + date1;}
         }
     }
